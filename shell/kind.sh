7591f9d6279123d88cf1714f8b31d5b132be5842
Megha Mehta <mmehta.ten@gmail.com>
2023-01-07 16:14:04 +0530

    feat: Support for ingress and ingress-creation script
    This adds script to add/delete ingress-nginx as ingress controller,
    along with a test ingress to verify

--- shell/kind.sh
+++ shell/kind.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 set -eux
-
 BASEDIR=$(dirname $0)
 echo $BASEDIR
 
 HOME_DIR=$PWD # HOME_DIR=$(dirname `dirname $PWD`)
 
-# mandatory scripts
-# find ./terragrunt/live  -type f -name terragrunt-debug.tfvars.json -exec rm {} +
-
   case $1 in
 	start)
-            # Run Init 
+            # Create a kind cluster
             kind create cluster --config $HOME_DIR/kind-config.yaml || true
             kubectl cluster-info --context kind-kind
 
+            # Create ingress
+            kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+            kubectl wait --namespace ingress-nginx \
+          --for=condition=ready pod \
+          --selector=app.kubernetes.io/component=controller \
+          --timeout=90s
+            ;;
+  apply) 
+            kubectl apply -f $HOME_DIR/test-ing.yaml
             ;;
 	delete)
             # Run Init for a single component directory
