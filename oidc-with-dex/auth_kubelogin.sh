bd6644d1573588fbba9a46eabcd4842b08bb4af2
Megha Mehta <megha.mehta@spearline.com>
2023-04-07 17:29:59 +0530

    fix: auth with kubelogin kubectl plugin

--- oidc-with-dex/auth_kubelogin.sh
+++ oidc-with-dex/auth_kubelogin.sh
@@ -6,18 +6,10 @@ echo $BASEDIR
 
 HOME_DIR=$PWD # HOME_DIR=$(dirname `dirname $PWD`)
 
-docker run -p 5557:5557 -d --network=kind -v ${PWD}:/data:Z -v ${PWD}/ssl:/ssl:ro --name oidc ghcr.io/dexidp/dex:v2.31.0 dex serve /data/dex.yaml
-
-echo $(docker inspect oidc -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}') oidc| sudo tee -a /etc/hosts
-
-sleep 4
-
-curl --cacert ssl/ca.pem https://oidc:5557/dex/.well-known/openid-configuration
-
 kubectl oidc-login setup \
   --oidc-issuer-url=https://oidc:5557/dex \
   --oidc-client-id=kubernetes \
-  --oidc-client-secret=ZXhhbXBsZS1hcHAtc2VjcmV0 \
+  --oidc-client-secret=SuperSecretSecret \
   --certificate-authority=${PWD}/ssl/ca.pem \
   --oidc-extra-scope=email
 
