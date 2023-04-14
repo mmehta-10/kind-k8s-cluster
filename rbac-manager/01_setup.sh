68877a1315c8163becc9ebf5765b3cbcb6fce5bf
Megha Mehta <megha.mehta@spearline.com>
2023-04-14 14:51:38 +0530

    feat: use oidc setup for authN before using rbac-manager

--- rbac-manager/01_setup.sh
+++ rbac-manager/01_setup.sh
@@ -3,12 +3,25 @@
 set -eux
 BASEDIR=$(dirname $0)
 echo $BASEDIR
-HOME_DIR=$PWD 
+HOME_DIR=$PWD
+
+# Start fake LDAP
+../oidc-with-dex/01_gencerts.sh
+../oidc-with-dex/02_ldap-start.sh
+
+# Create symlink to ../oidc-with-dex/ssl dir, for next step
+ln -sf ../oidc-with-dex/ssl ssl
+
+# Setup kind cluster with OIDC enabled
+../oidc-with-dex/kind.sh start
+
+# Setup OIDC with DEX+DEX_k8s_authenticator
+../oidc-with-dex/03_dex-start.sh
 
 # create namespace sandbox
 
-# create dummy user accounts 
+# create dummy user accounts
 
-# check groups of users 
+# check groups of users
 
-# create roles 
+# create roles
