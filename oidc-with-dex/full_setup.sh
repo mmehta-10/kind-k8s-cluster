64e1a92d66888a5fd8d9bedc548cf32188e202aa
Megha Mehta <megha.mehta@spearline.com>
2023-05-04 14:28:16 +0530

    feat: (semi) partial cluster setup for oidc dex

---
+++ oidc-with-dex/full_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+HOME_DIR=$PWD
+
+# ## Start fake LDAP
+# ../oidc-with-dex/01_gencerts.sh
+# ../oidc-with-dex/02_ldap-start.sh
+
+# Create symlink to ../oidc-with-dex/ssl dir, for next step
+# ln -sf ../oidc-with-dex/ssl ssl
+
+# Setup kind cluster with OIDC enabled
+$BASEDIR/../oidc-with-dex/kind.sh start
